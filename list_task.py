4a221ccdd3a287e20a7624f78c712695ba9c654f
Fahad Hussain <fahadhussainbh99@gmail.com>
2026-04-14 11:26:26 +0500

    list task

---
+++ list_task.py
@@ -0,0 +1,12 @@
+num = [2,4,6,8,10,12,14,16]
+print("First Element:",num[0])
+print("Last Element:",num[4])
+
+print("All Elements:")
+for x in num:
+    print(x)
+
+print("Nubers greater than 10:")
+for x in num:
+    if x > 10:
+        print(x)
