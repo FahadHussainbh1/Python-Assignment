2fbadc194f6390393903a71c633c1d946d19f5f0
Fahad Hussain <fahadhussainbh99@gmail.com>
2026-04-23 13:16:03 +0500

    Implement speed category classification in Car class
    Added speed category method to classify car speed.

--- Car_Task_2.py
+++ Car_Task_2.py
@@ -8,10 +8,15 @@ class Car:
         self.speed = speed
         
     def Display_Car(self):
-        print("Brand: " , self.brand , "|", "Color: " , self.color,"|", "Top Speed: " , self.speed)    
-
+        print("Brand: " , self.brand , "|", "Color: " , self.color,"|", "Top Speed: " , self.speed,"km/h", " | Speed Catagory",self.speed_catagory())    
+    def speed_catagory(self):
+        if self.speed>150:
+            return "Fast"
+        else:
+            return "Normal"
 
 car1 = Car("Toyota", "Red", 200)
 car1.Display_Car()
 car2 = Car("Honda", "Blue", 220)
 car2.Display_Car()
+
